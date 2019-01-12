74f91ef3146af3fb8da065cd48d38f170a105765
asmaamahmouhabash <asmaamahmoudhabash@gmail.com>
2019-01-12 23:23:32 +0200

    add post migration

---
+++ database/migrations/2019_01_12_211735_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->string('body');
+            $table->integer('access');
+            $table->tinyInteger('is_published');
+            $table->integer('order');
+            $table->integer('category_id');
+            $table->integer('user_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
